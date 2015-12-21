445734210239d9d773c9cc20ccf9d564468defcd
gogolB <sbhat006@ucr.edu>
2015-12-20 16:02:38 -0800

    Added in the basics of login.

--- includes/login_functions.php
+++ includes/login_functions.php
@@ -1,2 +1,55 @@
-TEsting login functions
+<?php
+  // ---------------------------------------------------------------------------
+  // Here all the functions required to log in are stored. The Sesson variables
+  // are set from here as well. These include User access level (User, Super,
+  // Admin, Transactor).
+  // ---------------------------------------------------------------------------
 
+  // This is a temp function. It is supposed to be replaced with whatever your
+  // login system is supposed to be.
+  function login($s_username, $s_password)
+  {
+    $_SESSION['Username'] = $$s_username;
+    if($s_username == "PinkiesAdmin")
+    {
+        setAdminPermissions();
+        return true;
+    }
+    elseif ($s_username == "PinkiesSuper")
+    {
+      setSupervisorPermissions();
+      return true;
+    }
+    elseif ($s_username == "PinkiesTrans")
+    {
+      setTransactorPermissions();
+      return true;
+    }
+    elseif ($s_username == "PinkiesUser")
+    {
+      setUserPermissions();
+      return true;
+    }
+    return false;
+  }
+
+  function setAdminPermissions()
+  {
+    $_SESSION['Access'] = "ADMIN";
+  }
+
+  function setSupervisorPermissions()
+  {
+    $_SESSION['Access'] = "SUPER";
+  }
+
+  function setTransactorPermissions()
+  {
+    $_SESSION['Access'] = "TRANS";
+  }
+
+  function setUserPermissions()
+  {
+    $_SESSION['Access'] = "USER";
+  }
+ ?>
--- onLogin.php
+++ onLogin.php
@@ -1,1 +1,31 @@
-This will handle the login and session generation.
+<?php
+  //----------------------------------------------------------------------------
+  // This script is responsible for checking if a given username and password is
+  // correct. Then it will generate the session variables necesary for logging
+  // in to and using the rest of ePinkies2.
+  //----------------------------------------------------------------------------
+
+  include 'includes/login_functions.php'
+  include 'includes/functions.php'
+
+  // Start the session.
+  secureSessionStart();
+
+  if(isset($_POST['username']) && isset($_POST['pwd']))
+  {
+    if(login($_POST['username'], $_POST['pwd']))
+    {
+      // Redirect to home page.
+      header("Location: home.php");
+    }
+    else
+    {
+      header("Location: ../");
+    }
+  }
+  else
+  {
+    header("Location: ../");
+  }
+
+ ?>
