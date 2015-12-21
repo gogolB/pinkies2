34f79123eb431a0ab46a5c099624940b5d8c6fc5
gogolB <sbhat006@ucr.edu>
2015-12-20 16:25:17 -0800

    Should now pass the session variables correctly.

--- onLogin.php
+++ onLogin.php
@@ -9,8 +9,8 @@
   include 'includes/functions.php';
 
   ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
 
   // Start the session.
   secureSessionStart();
