60790ed1aee498fd24033b0cc961ce26f763ab47
gogolB <sbhat006@ucr.edu>
2015-12-20 17:07:44 -0800

    Finished up the error page.
    Finished up the error page.

--- oops.php
+++ oops.php
@@ -10,7 +10,7 @@
     <title>Oops!</title>
     <style>
       .jumbotron {
-        background-color: #FFBABA;
+        background-color: #D8000C;
         color: #ffffff;
       }
     </style>
@@ -24,6 +24,13 @@
       <div class="well">
         <H2><?php echo($s_errorTitle);?></H2>
         <p><?php echo($s_errorReason);?></p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="card">
+        <H2>Please take a screenshot of this error report or explain your problem to <a href="mailto:systems@ece.ucr.edu?Subject=ePinkies2%20Error" target="_top">ECE Systems</a></H2>
+        <p>Click here to return to <a href"./home.php">home</a>.</p>
       </div>
     </div>
   </body>
