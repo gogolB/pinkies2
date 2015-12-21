b732118909562ca0477960beffbdcbcdfb363bd6
gogolB <sbhat006@ucr.edu>
2015-12-20 21:34:12 -0800

    Added a comment to function.js
    added a comment to function.js

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,8 @@
 // -----------------------------------------------------------------------------
 // Here you can find all the basic helper functions needed for the client.
 // -----------------------------------------------------------------------------
+
+// redirects the client to a specific url.
 function redirect( url )
 {
   window.location = url;
