3b1a11b6d60c41a7b8dba1c1e0595db6baaf2c63
gogolB <sbhat006@ucr.edu>
2015-12-25 13:16:00 -0800

    Started the first Part of the pinkie header.

--- newpinkie.php
+++ newpinkie.php
@@ -36,7 +36,38 @@ if(isset($_POST['title']))
     </div>
 
     <form class="form-horizontal" role="form" action="#" method="POST" name="newPinkieForm">
+      <!-- Title, who you are submitting to, who is submitting it. -->
+      <div class="container">
+        <div class="well">
 
+          <div class="form-group form-group-lg">
+            <label class="control-label col-sm-2" for="title">Title:</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="title" name="title" placeholder="Title of the pinkie" value="<?php echo $_POST['title']; ?>">
+            </div>
+          </div>
+
+          <div class="form-group form-group-lg">
+            <label class="control-label col-sm-2" for="submittedBy">Submitted By:</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="submittedBy" name="submittedBy" placeholder="Submitter" value="<?php echo getName(); ?>" readonly>
+            </div>
+          </div>
+
+          <div class="form-group form-group-lg">
+            <label class="control-label col-sm-2" for="submitTo">Submitted To:</label>
+            <div class="col-sm-10">
+              <select class="form-control" id="submitTo" name="submitTo">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+              </select>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </form>
 
   </body>
