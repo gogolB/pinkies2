f56af2e8a2243641536f334574e00623ae6a9ce4
gogolB <sbhat006@ucr.edu>
2015-12-25 19:48:02 -0800

    Added new files.

--- newpinkie.php
+++ newpinkie.php
@@ -68,6 +68,41 @@ if(isset($_POST['title']))
       var minFunds = 1;
       var currentFund = minFunds;
 
+
+      function addAObject()
+      {
+        if(currentObject < maxObject)
+        {
+          var div = document.createElement("div");
+              div.id = 'PurchaseObject'+ currentObject;
+              div.innerHTML = addObjectInput;
+              document.getElementById('moreObjects').appendChild(div);
+              currentObject++;
+              return false;
+        }
+        else
+        {
+          alert("Too many objects with this Pinkie.");
+        }
+      }
+
+      function addAFund()
+      {
+        if(currentFund < maxFunds)
+        {
+          var div = document.createElement("div");
+              div.id = 'Expense'+ currentFund;
+              div.innerHTML = addFundInput;
+              document.getElementById('moreFunds').appendChild(div);
+              currentFund++;
+              return false;
+        }
+        else
+        {
+          alert("Too many objects with this Pinkie.");
+        }
+      }
+      
     </script>
   </HEAD>
   <body>
