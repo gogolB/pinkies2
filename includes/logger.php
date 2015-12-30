7257af7036f6d11cc58367606eb8a553875060b3
gogolB <sbhat006@ucr.edu>
2015-12-29 21:45:52 -0800

    Started work on the logger.

---
+++ includes/logger.php
@@ -0,0 +1,44 @@
+<?php
+include_once 'functions.php';
+
+
+function log($pinkieID, $user, $msg, $lvl)
+{
+  $_db = getMysqli();
+
+  $_sql = "INSERT INTO Log (PinkieID, User, Level, Message) VALUES (?,?,?,?)";
+  $_stmt = $_db->prepare((string)$_sql);
+
+  $_stmt->bind_param('isss', $pinkieID, $user, $lvl, $msg);
+  $_stmt->execute();
+
+  if ($_stmt->errno)
+  {
+    onError("Error in Logger::log()", $_stmt->error);
+  }
+
+  $_stmt->close();
+  // Close up the database connection.
+  $_db->close();
+}
+
+function logGeneral($pinkieID, $user, $msg)
+{
+  log($pinkieID, $user, $msg, "General");
+}
+
+function logWarning($pinkieID, $user, $msg)
+{
+  log($pinkieID, $user, $msg, "Warning");
+}
+
+function logDanger($pinkieID, $user, $msg)
+{
+  log($pinkieID, $user, $msg, "Danger");
+}
+
+function logError($pinkieID, $user, $msg)
+{
+  log($pinkieID, $user, $msg, "Error");
+}
+?>
