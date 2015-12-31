f50c1bf6a893d79085f83f6973aff0fa48c134da
gogolB <sbhat006@ucr.edu>
2015-12-30 17:02:33 -0800

    Add all other pinkies.

--- viewallpinkies.php
+++ viewallpinkies.php
@@ -105,6 +105,57 @@
     $_db->close();
   }
 
+  function printAllOthersTable()
+  {
+    $_db = getMysqli();
+    $statement = $_db->prepare("SELECT * FROM Submitted_By WHERE Submitter!=? && SubmittedFor!=?");
+    $statement->bind_param('ss', $_SESSION['Username'], $_SESSION['Username']);
+    $statement->execute();
+
+    // Error running the statement.
+    if($statement->errno != 0)
+    {
+      $_tmp = $statement->error;
+      $statement->close();
+      $_db->close();
+      onError("ViewAllPinkies::printAllOthersTable()",'There was an error running the query [' . $_tmp . '] Could not fetch Pinkies submitted to: '.$_SESSION['Username']);
+    }
+
+    $statement->store_result();
+    if($statement->num_rows <= 0)
+    {
+        echo '<tr>
+                <td> No OTher Pinkies!</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>';
+        return;
+    }
+    // We have a result, lets bind the result to the variables.
+    $statement->bind_result($pinkieID, $timestamp, $submitterUser, $submittedFor, $title, $status, $totalvalue);
+    while($statement->fetch())
+    {
+        printf('<tr>
+                <td>%s</td>
+                <td>%s</td>
+                <td>%s</td>
+                <td>%s</td>
+                <td>%.2f</td>
+                <td>%s</td>
+                <td><a href="./viewpinkie.php?pid=%d" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-search"></span> View</a></td>
+              </tr>', $title, $submitterUser, $submittedFor, $timestamp, $totalvalue, $status, $pinkieID);
+    }
+
+    // Cleanup.
+    $statement->free_result();
+    $statement->close();
+    $_db->close();
+  }
+
   ?>
   <!DOCTYPE html>
   <html>
@@ -179,5 +230,32 @@
            </div>
          </div>
        </div>
+
+       <?php if(isAdmin()) : ?>
+       <div class="container">
+         <div class="well">
+           <H2><u>All Other Pinkies</u></H2>
+           <div class="table-responsive">
+             <table class="table table-bordered table-hover table-condensed">
+               <thead>
+                 <tr>
+                   <th>Pinkie Title</th>
+                   <th>Submitted By</th>
+                   <th>Submitted To</th>
+                   <th>Timestamp</th>
+                   <th>Total</th>
+                   <th>Status</th>
+                   <th>Options</th>
+                 </tr>
+               </thead>
+               <tbody>
+                 <?php printAllOthersTable(); ?>
+               </tbody>
+             </table>
+           </div>
+         </div>
+       </div>
+     <?php endif; ?>
+
      </body>
 </html>
