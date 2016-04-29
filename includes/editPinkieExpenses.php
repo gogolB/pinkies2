046eead719a68e459d490da10535b9016a630515
Souradeep "Gogol" Bhattacharya <sbhattacharya@engr.local>
2016-04-29 11:54:41 -0700

    Added in delete functionality

--- includes/editPinkieExpenses.php
+++ includes/editPinkieExpenses.php
@@ -4,7 +4,21 @@ secureSessionStart();
 
 if(strcmp($_POST['mode'], "delete") == 0) // We are in delete mode.
 {
-  echo "delete mode!";
+  $_db = getMysqli();
+  $_sql = "DELETE FROM Expenses WHERE ExpenseID=?";
+  $_stmt = $_db->prepare((string)$_sql);
+
+  $_stmt->bind_param('i', $_POST['expenseID']);
+  $_stmt->execute();
+
+  if ($_stmt->errno)
+  {
+    onError("editPinkieExpenses::Delete()", $_stmt->error);
+  }
+  $_stmt->close();
+  // Close up the database connection.
+  $_db->close();
+  echo "OKAY";
   return;
 }
 if(strcmp($_POST['mode'], "edit") == 0) // We are in edit mode.
