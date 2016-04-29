74ee6dcfddebc8d6cb9535bed778d1704caadf52
Souradeep "Gogol" Bhattacharya <sbhattacharya@engr.local>
2016-04-29 12:05:59 -0700

    added in the printing of variables.

--- includes/editPinkieExpenses.php
+++ includes/editPinkieExpenses.php
@@ -4,6 +4,10 @@ secureSessionStart();
 
 if(strcmp($_POST['mode'], "delete") == 0) // We are in delete mode.
 {
+  // TESTING Only
+  print_r($_POST);
+  return;
+
   $_db = getMysqli();
   $_sql = "DELETE FROM Expenses WHERE ExpenseID=?";
   $_stmt = $_db->prepare((string)$_sql);
@@ -23,12 +27,16 @@ if(strcmp($_POST['mode'], "delete") == 0) // We are in delete mode.
 }
 if(strcmp($_POST['mode'], "edit") == 0) // We are in edit mode.
 {
+  print_r($_POST);
+  return;
   echo "edit mode!";
   return;
 
 }
 if(strcmp($_POST['mode'], "add") == 0) // We are in add mode.
 {
+  print_r($_POST);
+  return;
   echo "add mode!";
   return;
 }
